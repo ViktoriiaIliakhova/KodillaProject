edf46db59416fb89ff4af2d6bf1701f7272e1549
John Doe <johndoe@example.com>
2022-12-28 16:27:12 +0100

    Add total amount

--- qwerty.py
+++ qwerty.py
@@ -5,3 +5,6 @@ product_dictionary = {
 for key, val in product_dictionary.items():
     val = [i.title() for i in val]
     print(f"I am going to {key.capitalize()} and buy there {val}.")
+    
+print("Total number of products:")
+print(sum([len(product_dictionary[val]) for val in product_dictionary]))
