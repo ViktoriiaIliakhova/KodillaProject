c0eb904eee267920b02f1257e8c014457c62d148
John Doe <johndoe@example.com>
2022-12-28 15:46:47 +0100

    Add capitalize method

--- qwerty.py
+++ qwerty.py
@@ -2,3 +2,6 @@ product_dictionary = {
     "bakery": ["bread", "donuts", "buns"],
     "grocery_store": ["carrot", "celery", "arugula"]
 }
+for key, val in product_dictionary.items():
+    val = [i.title() for i in val]
+    print(f"I am going to {key.capitalize()} and buy there {val}.")
