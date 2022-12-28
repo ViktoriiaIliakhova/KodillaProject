774392b4c02f278c0b2cd9d257c2058fc742986b
John Doe <johndoe@example.com>
2022-12-28 15:41:15 +0100

    Add product_dictionary

--- qwerty.py
+++
@@ -1,7 +0,0 @@
-product_dictionary = {
-    "bakery": ["bread", "donuts", "buns."],
-    "grocery_store": ["carrot", "celery", "arugula."]
-}
-for key, val in product_dictionary.items():
-    val = [i.title() for i in val]
-    print(f'I am going to {key.capitalize()} and buy there {val}')
