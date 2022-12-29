ea83b0c26a0da160a262427a172a5a62385196b9
John Doe <johndoe@example.com>
2022-12-29 12:31:06 +0100

    Change dictionary name

--- qwerty.py
+++ qwerty.py
@@ -1,10 +1,10 @@
-product_dictionary = {
+product_dictionary12 = {
     "bakery": ["bread", "donuts", "buns"],
     "grocery_store": ["carrot", "celery", "arugula"]
 }
-for key, val in product_dictionary.items():
+for key, val in product_dictionary12.items():
     val = [i.title() for i in val]
     print(f"I am going to {key.capitalize()} and buy there {val}.")
     
 print("Total number of products:")
-print(sum([len(product_dictionary[val]) for val in product_dictionary]))
+print(sum([len(product_dictionary12[val]) for val in product_dictionary12]))
